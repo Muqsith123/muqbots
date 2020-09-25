827a59b7b30a7f4e517a387acf6f82f34c272225
Muqsith123 <muqsithkediri123@gmail.com>
2020-09-25 14:36:13 +0700

    WEW

--- commands/hari.js
+++ commands/hari.js
@@ -4,6 +4,6 @@ module.exports = {
     name: 'hari',
     description: 'dino opo iki',
     execute(message) {
-        message.channel.send("Sekarang" + moment().format('D MMMM YYYY, h:mm:ss a'));
+        message.channel.send("Sekarang " + moment().format('D MMMM YYYY, h:mm:ss a'));
     }
 }
