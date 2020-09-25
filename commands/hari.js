0ba7b2eeb9e472cdbcc7178f9588310449562d99
Muqsith123 <muqsithkediri123@gmail.com>
2020-09-25 14:33:52 +0700

    TEST

---
+++ commands/hari.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+module.exports = {
+    name: 'hari',
+    description: 'dino opo iki',
+    execute(message) {
+        message.channel.send("Sekarang" + moment().format('D MMMM YYYY, h:mm:ss a'));
+    }
+}
