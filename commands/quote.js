dcb3da856955b8c4e3d5e6fdd5f4d7b6f40d9a46
Muqsith123 <muqsithkediri123@gmail.com>
2020-10-01 16:52:02 +0700

    Tambahan Terjemahan Indo

--- commands/quote.js
+++ commands/quote.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
+const translate = require("@vitalets/google-translate-api")
 
 module.exports = {
     name: 'quote',
@@ -10,11 +11,21 @@ module.exports = {
         .then(data => {
             let quotenya = data.quote.toLocaleString()
             let author = data.author.toLocaleString()
-            const embed = new MessageEmbed()
-            .setDescription('"' + quotenya + '"')
-            .setAuthor(author)
-            .setColor('#00f1ff')
+            
+            translate(quotenya, {to: 'id'}).then(res => {
+                   let terjemahan = res.text
+
+                const embed = new MessageEmbed()
+                .setDescription('"' + quotenya + '"')
+                .setAuthor(author)
+                .setColor('#00f1ff')
+                .addField('Di Terjemahan Ke Bahasa Indo : ', terjemahan, true)
+
             message.channel.send(embed)
+                }).catch(err => {
+                    console.error(err);
+                });
+
         }).catch(e => {
             return message.channel.send('Terjadi Error. Mohon Coba Lagi Nanti !')
         })
