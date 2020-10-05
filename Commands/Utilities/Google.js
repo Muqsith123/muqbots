5bc849d9c621906010f2eaca189b85eabfd1940f
Muqsith123 <muqsithkediri123@gmail.com>
2020-10-05 08:38:58 +0700

    pembaruan

---
+++ Commands/Utilities/Google.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const request = require('node-superfetch');
+
+module.exports = {
+    name: 'google',
+    description: 'mencari isi web',
+    run: async(bot, message, args) => {
+        const googlekey = process.env.GOOGLE_API;
+        const csx = '5bc857784fb9d1718';
+        let query = args.join(" ");
+        let result;
+
+        if(!query) return message.channel.send("Mohon Masukkan Pencarian !");
+
+        href = await search(query)
+        if(!href) return message.channel.send("Pencarian Tidak Di Temukan !");
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle(href.title)
+        .setDescription(href.snippet)
+        .setImage(href.pagemap ? href.pagemap.cse_thumbnail[0].src : null)
+        .setURL(href.link)
+        .setColor('#00f1ff')
+        .setFooter('Powered By Google')
+
+        return message.channel.send(embed)
+
+        async function search(query) {
+            const { body } = await request.get("https://www.googleapis.com/customsearch/v1").query({
+                key: googlekey, cx: csx, safe: 'off', q: query
+            })
+        if(!body.items) return null;
+        return body.items[0];
+        }
+    }
+}
