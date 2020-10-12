9dc9626b5f4e434f89d6cc92fe5bee27c11e7717
Muqsith123 <muqsithkediri123@gmail.com>
2020-10-12 23:10:31 +0700

    carmeet

--- Core/Carmeet.js
+++ Core/Carmeet.js
@@ -4,12 +4,31 @@ module.exports = async (bot, message) => {
     if(message.author.bot) return;
     if (message.channel.id != "764731128804278272") return; 
       if(message.content === "Saya Siap Ikut Carmeet") {
-        if (message.member.roles.cache.get('730010922369679440')) return message.reply('Anda Sudah Terdaftar !');
-        if (message.member.roles.cache.get('738395119992176670')) return message.reply('Anda Sudah Terdaftar !');
-        if (message.member.roles.cache.get('748073934745763922')) return message.reply('Anda Sudah Terdaftar !');
-        if (message.member.roles.cache.get('764743128032608288')) return message.reply('Anda Sudah Terdaftar !');
+
+        //Mengecek Roles Apakah Sudah Ikut Carmeet
+        let sudahikut;
+
+        let founder = '730010922369679440'
+        let coadmin = '738395119992176670'
+        let staff = '748073934745763922'
+        let carmeet1 = '764709773643218954'
+        let carmeet2 = '764743128032608288'
+
+        let list_terdaftar = [founder,
+                              coadmin,
+                              staff,
+                              carmeet2]
+
+        list_terdaftar.forEach(getrole => {
+          if (message.member.roles.cache.get(getrole)) return sudahikut = true;
+        })
+        if (message.member.roles.cache.get(carmeet1)) return message.reply('Anda Sudah Mengikuti Carmeet Sebelumnya !');
+        
+        if (sudahikut) return message.reply('Anda Sudah Terdaftar !')
         if (a === 15) return message.reply('Maaf Sudah Tercapai limit Lain Kali Aja Ya !')
-          let role = message.guild.roles.cache.get('764709773643218954')
+
+        //Jika Belum Daftar Maka Akan Melanjutkan Ini :
+          let role = message.guild.roles.cache.get(carmeet2)
           let member = message.author.id
           const target = message.guild.members.cache.get(member);
           a++
