d822239696ec6f5e0f156cd57caf85c78cdac0bd
Muqsith123 <muqsithkediri123@gmail.com>
2020-11-01 12:40:31 +0000

    Adding Exec Command

---
+++ Commands/DevOnly/Exec.js
@@ -0,0 +1,36 @@
+const process = require('child_process');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'exec',
+  category: 'DevOnly',
+  description: 'Exec Command For Developer !',
+  ownerOnly: true,
+  run: async(bot, message, args) => {
+    let input = args.join(" ")
+
+    let waiting = await message.channel.send('Please Wait...');
+
+    process.exec(input, (error, stdout) => {
+      let result = (error || stdout);
+      let embed = new MessageEmbed()
+      .setColor('RANDOM')
+      .addFields(
+        {name: 'Input : ', value: '``\`'+input+'``\`', inline: false}
+        );
+      embed.addFields(
+        {name: 'Result : ', value: '``\`'+result+'``\`', inline: false}
+        )
+      waiting.delete()
+      message.channel.send(embed).catch(err => {
+      embed.addFields(
+        {name: 'Error : ', value: err, inline: false}
+        )
+      waiting.delete()
+      message.channel.send(embed)
+      }) 
+    })
+
+    return;
+  }
+}
