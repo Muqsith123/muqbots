90b91394bd2909d0b9215853819de41f26191a3f
Muqsith123 <muqsithkediri123@gmail.com>
2020-11-05 22:41:46 +0700

    Adding Imgur

--- Commands/Fun/Imgur.js
+++ Commands/Fun/Imgur.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: 'imgur',
@@ -8,12 +9,22 @@ module.exports = {
     let query = args.join(" ");
     if(!query) return message.channel.send('Please Input The Query !');
 
-    const url = `https://api.imgur.com/3/gallery/search/top/1/?q=${query}`
+    const url = `https://api.imgur.com/3/gallery/search/top/1/?q=${query}&q_type=jpg,jpeg,png`
     const api = process.env.IMGUR;
 
     fetch(url, {headers: {Authorization: `Client-ID ${api}`}})
     .then(res => res.json())
-    .then(json => console.log(json))
+    .then(json => {
+      let id = Math.floor(Math.random() * json.data.length)
+      let embed = new MessageEmbed()
+      .setTitle(json.data[id].title)
+      .setImage(json.data[id].images[0].link)
+      .setColor('RANDOM')
+      .setURL(json.data[id].link)
+      .setFooter(`Author : ${json.data[id].account_url}`)
+
+      message.channel.send(embed)
+    })
 
 
   }
