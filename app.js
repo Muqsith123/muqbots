5cce0b69319ce9179309e69fbc201e2adb43aa75
Muqsith123 <muqsithkediri123@gmail.com>
2020-11-05 09:01:53 +0700

    Update

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-const express = require('express');
-const server = express();
-
-server.all('/', (req, res)=>{
-    res.send('Your bot is alive!')
-})
-
-function keepAlive(){
-    server.listen(3000, ()=>{console.log("Server is Ready!")});
-}
+const keepAlive = require('./server.js')
 keepAlive()
 
 const Discord = require('discord.js');
